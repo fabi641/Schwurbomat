6737ee5223f2a985ba019d5955a879595c83f600
Fabian Hartmann <git@hartmann.red>
2022-01-26 17:20:09 +0000

    threw out python scheduler
    so now just cronjob or whatever the scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,20 +1,15 @@
-import schedule
+#!/usr/bin/env python3
+
 import time
 import tweet_a_schwurbel
 
-i = 0
-Zeit = "15:00"
 
 #tweetet einen schwurbel tweet und schreibt ihn auch nochmal in die shell
 def schwurbeln():
     print("Tweet this:\n")
     print("-------------------------------------")
-    tweet_a_schwurbel.tweet()
+#    tweet_a_schwurbel.tweet()
     print("-------------------------------------")
-
-
-#Jeden Tag um 15:00 Uhr wird 'schwurbeln' ausgeführt
-schedule.every().day.at(Zeit).do(schwurbeln)
 
 #Hat das Twitter-API-Gedöns geklappt?
 print("Twitter Api Kram:")
@@ -22,21 +17,7 @@ print(tweet_a_schwurbel.api.VerifyCredentials())
 print("---------------------------------------------\n")
 
 #Am Anfang was ausgeben
-print("Jeden Tag um "+ Zeit +" Uhr gibt's Geschwurbel!")
 print("Starting Geschwurbel...")
 print("Es ist: "+ time.strftime("%d.%m.%Y %H:%M:%S"))
 
-#Dauerschleife
-while True:
-    schedule.run_pending()
-    time.sleep(60) # wait one minute
-    #Immer mal schreiben, dass das skript noch lebt
-    i += 1
-    if i >= 10:
-	#um die Connection zur twitter API aufrecht zu erhalten
-        try:
-            tweet_a_schwurbel.api.VerifyCredentials()
-            print("Es ist: "+ time.strftime("%d.%m.%Y %H:%M:%S"))
-            i = 0
-        except:
-            print("Beim Aufrechterhalten der Verbindung ist ein Fehler aufgetreten.\n In einer Minute wird es nochmal probiert")
+schwurbeln()
